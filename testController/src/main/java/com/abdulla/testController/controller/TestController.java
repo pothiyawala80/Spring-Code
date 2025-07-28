3e9ed0a6b5c2c910c16bcda64159d47c1aabf3e3
AbdullaP <pothiyawala80@gmail.com>
2025-07-29 01:22:33 +0530

    Adding 1st code

---
+++ testController/src/main/java/com/abdulla/testController/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.abdulla.testController.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	@Value("${application.properties.source}")
+	String application_properties_source;
+	
+	@GetMapping("/version")
+	public String getControllerVersion() {
+		return "This is version 1 and properties source is : "+application_properties_source;
+	}
+}
